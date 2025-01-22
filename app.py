510b5a9dfd87b89c18bdacb90e4d5cd278d9881a
charles <ch4rle2.fh@gmail.com>
2025-01-21 22:04:31 -0300

    Ajuste no acesso às credenciais do Firebase usando st.secrets

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
 import streamlit as st
+import os
 from firebase.firebase_utils import initialize_firebase, set_data, get_data
 
-# Caminho para as credenciais e URL do banco
-CRED_PATH = "firebase_credentials.json"
-DATABASE_URL = "https://bikepacking-tracker-default-rtdb.firebaseio.com"
+# Obtém as credenciais da variável de ambiente
+cred_json_str = st.secrets["firebase"]["credentials"]
 
-# Inicializar Firebase
-initialize_firebase(CRED_PATH, DATABASE_URL)
+if cred_json_str is None:
+    st.error("As credenciais do Firebase não foram encontradas. Verifique a configuração.")
+else:
+    # URL do banco de dados
+    DATABASE_URL = "https://bikepacking-tracker-default-rtdb.firebaseio.com"
 
-# Interface de usuário
-st.title("Bikepacking Tracker")
+    # Inicializar Firebase
+    initialize_firebase(cred_json_str, DATABASE_URL)
 
-# Formulário para enviar dados
-with st.form("form_percurso"):
-    distancia = st.number_input("Distância (km)", min_value=0)
-    altimetria = st.number_input("Altimetria (m)", min_value=0)
-    tempo = st.text_input("Tempo estimado (hh:mm)")
-    if st.form_submit_button("Enviar"):
-        data = {
-            "distancia": distancia,
-            "altimetria": altimetria,
-            "tempo": tempo,
-        }
-        set_data("percurso/custom", data)
-        st.success("Dados enviados para o Firebase!")
+    # Interface de usuário
+    st.title("Bikepacking Tracker")
 
+    # Formulário para enviar dados
+    with st.form("form_percurso"):
+        distancia = st.number_input("Distância (km)", min_value=0)
+        altimetria = st.number_input("Altimetria (m)", min_value=0)
+        tempo = st.text_input("Tempo estimado (hh:mm)")
+        if st.form_submit_button("Enviar"):
+            data = {
+                "distancia": distancia,
+                "altimetria": altimetria,
+                "tempo": tempo,
+            }
+            set_data("percurso/custom", data)
+            st.success("Dados enviados para o Firebase!")
 
-# Obter dados do Firebase
-data = get_data("percurso/custom")
+    # Obter dados do Firebase
+    data = get_data("percurso/custom")
